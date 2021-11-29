a91922b675d9edb1e77ce92011696c602197029f
Brenda <brendaprofiro@gmail.com>
2021-11-28 22:10:57 -0300

    chore: Update API route

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://teapoio-env.eba-bmyypvgp.us-east-2.elasticbeanstalk.com/',
+  baseURL: 'https://provi-teajudamos.herokuapp.com/',
 });
 
 export default api;
